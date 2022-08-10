9480168320b027aea2959d2f917a22a9255f6137
userChenJiawei <15316120560@163.com>
2022-08-10 22:27:04 +0800

    fin

--- src/utils/zip.ts
+++ src/utils/zip.ts
@@ -1,22 +1,32 @@
-import { saveAs } from 'file-saver'
+import { saveAs } from "file-saver";
 // import JSZip from 'jszip'
-import JSZip = require('jszip');
+let JSZip = require("jszip");
 
-export function exportTxt2Zip(th: any, jsonData: any ,txtName = 'file', zipName = 'file') {
-  const zip = new JSZip()
-  const data = jsonData
-  let txtData = `${th}\r\n`
+export function exportTxt2Zip(
+  th: any,
+  jsonData: any,
+  txtName = "file",
+  zipName = "file"
+) {
+  const zip = new JSZip();
+  const data = jsonData;
+  let txtData = `${th}\r\n`;
   data.forEach((r: { toString: () => string }) => {
-    let tempStr = ''
-    tempStr = r.toString()
-    txtData += `${tempStr}\r\n`
-  })
-  zip.file(`${txtName}.txt`, txtData)
-  zip.generateAsync({
-    type: 'blob'
-  }).then((blob: any) => {
-    saveAs(blob, `${zipName}.zip`)
-  }, () => {
-    alert('Zip export failed:')
-  })
+    let tempStr = "";
+    tempStr = r.toString();
+    txtData += `${tempStr}\r\n`;
+  });
+  zip.file(`${txtName}.txt`, txtData);
+  zip
+    .generateAsync({
+      type: "blob",
+    })
+    .then(
+      (blob: any) => {
+        saveAs(blob, `${zipName}.zip`);
+      },
+      () => {
+        alert("Zip export failed:");
+      }
+    );
 }
