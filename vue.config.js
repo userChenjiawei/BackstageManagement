4913e151b1336d5e3a29591f258c2f3141231bc8
userChenJiawei <15316120560@163.com>
2022-08-10 23:23:33 +0800

    a

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,24 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV == "production" ? "./" : "/",
-  // outputDir: 'dist',
-  // runtimeCompiler: true,
-  chainWebpack: (config) => {
-    config.when(process.env.NODE_ENV === "production", (config) => {
-      config.entry("app").clear().add("./src/main-prod.js");
-      config.set("externals", {
-        moment: "moment",
-        // 'vue-router': 'VueRouter',
-        // axios: 'axios',
-        lodash: "_",
-        echarts: "echarts",
-        // nprogress: 'nprogress'
-        // 'vue-quill-editor': 'VueQuillEditor'
-      });
-    });
-    config.when(process.env.NODE_ENV === "development", (config) => {
-      config.entry("app").clear().add("./src/main-dev.js");
-    });
-  },
+  // publicPath: process.env.NODE_ENV == "production" ? "./" : "/",
+  // // outputDir: 'dist',
+  // // runtimeCompiler: true,
+  // chainWebpack: (config) => {
+  //   config.when(process.env.NODE_ENV === "production", (config) => {
+  //     config.entry("app").clear().add("./src/main-prod.js");
+  //     config.set("externals", {
+  //       moment: "moment",
+  //       // 'vue-router': 'VueRouter',
+  //       // axios: 'axios',
+  //       lodash: "_",
+  //       echarts: "echarts",
+  //       // nprogress: 'nprogress'
+  //       // 'vue-quill-editor': 'VueQuillEditor'
+  //     });
+  //   });
+  //   config.when(process.env.NODE_ENV === "development", (config) => {
+  //     config.entry("app").clear().add("./src/main-dev.js");
+  //   });
+  // },
   lintOnSave: false,
   configureWebpack: {
     resolve: { extensions: [".ts", ".tsx", ".js", ".json"] },
