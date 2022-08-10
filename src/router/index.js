6537fab1048b8f3df70e13d94793fdeed8012956
userChenJiawei <15316120560@163.com>
2022-08-10 22:56:05 +0800

    oooo

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 // import axios from 'axios'
-import {createRouter,createWebHistory} from 'vue-router'
-let routeList = []
-function importAll(r){
-  r.keys().forEach((key)=>routeList.push(r(key).default))
+import { createRouter, createWebHashHistory } from "vue-router";
+let routeList = [];
+function importAll(r) {
+  r.keys().forEach((key) => routeList.push(r(key).default));
 }
-importAll(require.context('./',true,/\.router\.js/))
+importAll(require.context("./", true, /\.router\.js/));
 console.log(routeList);
 // import Login from '../components/Login'
-const Login = () => import(/* webpackChunkName:"login_home_welcome" */ '../components/Login')
+const Login = () =>
+  import(/* webpackChunkName:"login_home_welcome" */ "../components/Login");
 // import Home from '../components/Home'
-const Home = () => import(/* webpackChunkName:"login_home_welcome" */ '../components/Home')
+const Home = () =>
+  import(/* webpackChunkName:"login_home_welcome" */ "../components/Home");
 // import Welcome from '../components/Welcome'
 // const Welcome = () => import(/* webpackChunkName:"login_home_welcome" */ '../components/Welcome')
 // import Users from '../components/user/Users'
@@ -34,42 +36,39 @@ const Home = () => import(/* webpackChunkName:"login_home_welcome" */ '../compon
 // // import Report from '../components/report/Report'
 // const Report = () => import(/* webpackChunkName:"Order_Report" */ '../components/report/Report')
 
-
 const routes = [
-    { path: '/', redirect: '/login' },
-    { path: '/login', component: Login },
-    {
-      path: '/home',
-      component: Home,
-      redirect: '/welcome',
-      children: [
-    //     // { path: '/welcome', component: Welcome },
-    //     // { path: '/users', component: Users },
-    //     // { path: '/rights', component: Rights },
-    //     // { path: '/roles', component: Roles },
-    //     // { path: '/categories', component: Cates },
-    //     // { path: '/params', component: Params },
-    //     // { path: '/goods', component: Goods },
-    //     // { path: '/goods/add', component: Add },
-    //     // { path: '/orders', component: Order },
-    //     // { path: '/reports', component: Report }
-    ...routeList
-      ]
-
-    }
-
-  ]
+  { path: "/", redirect: "/login" },
+  { path: "/login", component: Login },
+  {
+    path: "/home",
+    component: Home,
+    redirect: "/welcome",
+    children: [
+      //     // { path: '/welcome', component: Welcome },
+      //     // { path: '/users', component: Users },
+      //     // { path: '/rights', component: Rights },
+      //     // { path: '/roles', component: Roles },
+      //     // { path: '/categories', component: Cates },
+      //     // { path: '/params', component: Params },
+      //     // { path: '/goods', component: Goods },
+      //     // { path: '/goods/add', component: Add },
+      //     // { path: '/orders', component: Order },
+      //     // { path: '/reports', component: Report }
+      ...routeList,
+    ],
+  },
+];
 const router = createRouter({
-    history: createWebHistory() ,   //路由模式
-    routes
-})
+  hash: createWebHashHistory(), //路由模式
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
-    // next() 放行 next('/login')强制跳转
-    if (to.path === '/login') return next()
-    const tokenStr = window.sessionStorage.getItem('token')
-    if (!tokenStr) return next('/login')
-    next()
-  })
+  // next() 放行 next('/login')强制跳转
+  if (to.path === "/login") return next();
+  const tokenStr = window.sessionStorage.getItem("token");
+  if (!tokenStr) return next("/login");
+  next();
+});
 
-  export default router
+export default router;
